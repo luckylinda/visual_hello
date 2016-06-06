fa150169b8866eaf89d3e5ce9b08f2470dea966e
luckylinda <ilinda91@naver.com>
2016-06-06 12:15:22 +0900

    github for git pull

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,6 +12,7 @@ namespace HelloWorld
         {
             Console.WriteLine("World of GitHub!");
             Console.WriteLine("World of Linda!");
+            Console.WriteLine("Luna on Github");
         }
     }
 }
